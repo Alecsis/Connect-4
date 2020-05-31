76f2db87d0530b39476c91b9b912507dde9db218
AlexisF <alexisfabre.dev@gmail.com>
2020-05-31 23:51:26 +0200

    Environment added

--- gym_connect4/envs/connect4_env.py
+++ gym_connect4/envs/connect4_env.py
@@ -1,18 +1,93 @@
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+import numpy as np
 
 
-class Connect4Env(gym.Env):
-    metadata = {'render.modes': ['human']}
-    
+class ConnectFour(object):
+
     def __init__(self):
-        pass
-    def step(self, action):
-        pass
+
+        self.rows = 6
+        self.columns = 7
+
+        # Save the board state
+        self.obs = np.zeros((self.rows, self.columns), dtype=int)
+
+        # Learn about spaces here: http://gym.openai.com/docs/#spaces
+        self.action_space = spaces.Discrete(self.columns)
+
+        self.observation_space = spaces.Box(low=-1, high=1,
+                                            shape=(self.rows, self.columns),
+                                            dtype=np.int)
+
+        # Tuple corresponding to the min and max possible rewards
+        self.reward_range = (-10, 1)
+
+        # StableBaselines throws error if these are not defined
+        self.spec = None
+        self.metadata = None
+
     def reset(self):
-        pass
-    def render(self, mode='human'):
-        pass
-    def close(self):
-        pass
+        """
+        Reinitialize the environment to the initial state
+
+        :return: state
+        """
+
+        self.obs = np.zeros((self.rows, self.columns), dtype=int)
+
+    def valid(self, action) -> bool:
+        """ If we have a space then the move is valid"""
+        if self.obs[0, action] == 0:
+            return True
+        return False
+
+    def draw(self):
+        """
+        Visualize in the console or graphically the current state
+        """
+        print("+---" * 7 + '+')
+        for row in range(self.rows):
+            print(
+                '| ' + ' | '.join(list(map(str, list(self.obs[row, ::])))) + ' |')
+            print("+---" * self.columns + '+')
+
+    def step(self, action):
+        """
+        One can make a step on the environment and obtain its reaction:
+        - the new state
+        - the reward of the new state
+        - should we continue the game?
+
+        :return: state, reward, game_over, info
+        """
+
+        # Check if agent's move is valid
+
+        is_valid = self.valid(action)
+
+        if is_valid:  # Play the move
+
+            i = 1
+
+            while self.obs[-i, action] != 0:
+                i += 1
+
+            self.obs[-i, action] = 1
+
+            # TODO :  -> ADD THE OPPONENT AGENT
+
+            # TODO : COMPUTE THE REWARD AND IF THE GAME IS OVER
+
+            reward = 1 / 42
+
+            done = False
+
+            info = {}
+
+        else:  # End the game and penalize agent
+            reward, done, info = -10, True, {}
+
+        return self.obs, reward, done, info
+
